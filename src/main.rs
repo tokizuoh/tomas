54a90f1009ffb28a263e5e5751ebeee43ab12bf0
tokizuoh <tokizuoh@gmail.com>
2023-09-02 15:39:12 +0900

    add: parse

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ use clap::Parser;
 struct Input {
     text: String,
 }
+
+fn parse(text: &str) -> String {
+    text.to_string()
+}
+
 fn main() {
     let args: Input = Input::parse();
 }
+
+#[test]
+fn test_parse_single_line() {
+    let actual = parse("# Hello");
+    let expected = "Hello";
+    assert_eq!(actual, expected)
 }
