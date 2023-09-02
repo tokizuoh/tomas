ecf9b5ffe246d6e4e17d61a6541de86e66846b1c
tokizuoh <tokizuoh@gmail.com>
2023-09-02 16:04:22 +0900

    feat: remove head sharp & heat white spaces

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,31 @@ struct Input {
 }
 
 fn parse(text: &str) -> String {
-    text.to_string()
+    if text.starts_with('#') {
+        let text_without_start_sharp = &text[1..];
+        let text_without_start_white_spaces = text_without_start_sharp.trim_start();
+        text_without_start_white_spaces.to_string()
+    } else {
+        // TODO: except '#'
+        text.to_string()
+    }
 }
 
 fn main() {
     let args: Input = Input::parse();
+    let output = parse(&args.text);
+    println!("{}", output);
 }
 
 #[test]
-fn test_parse_single_line() {
+fn test_parse_single_line_includes_sharp() {
+    let actual = parse("#Hello");
+    let expected = "Hello";
+    assert_eq!(actual, expected)
+}
+
+#[test]
+fn test_parse_single_line_includes_sharp_and_white_space() {
     let actual = parse("# Hello");
     let expected = "Hello";
     assert_eq!(actual, expected)
